81d7479861b0596831e55129b0cc2b2e2a76e2dd
Nick Vazquez <nmv@iastate.edu>
2022-11-12 20:06:16 -0600

    Batter callbacks

--- 288GUI/App.py
+++ 288GUI/App.py
@@ -2,6 +2,7 @@ import logging
 import tkinter
 import tkinter as tk
 import Components.NavBar as NavBar
+from Models.NavBarCallbacks import *
 
 app_screen_width_pct = 50
 app_screen_height_pct = 50
@@ -17,7 +18,16 @@ def main():
 
     window = tk.Frame(root)
 
-    navbar = NavBar.NavBar(window)
+    nav_callbacks = NavSectionCallbacks(lambda x=0: print('Home'),
+                                        lambda x=0: print('Console'),
+                                        lambda x=0: print('About'))
+    control_callbacks = ControlSectionCallbacks(lambda x=0: print('Start'),
+                                                lambda x=0: print('Stop'),
+                                                lambda x=0: print('Pause'),
+                                                lambda x=0: print('Reset'),
+                                                lambda x=0: print('EStop'))
+    navbar_callbacks = NavBarCallbacks(nav_callbacks, control_callbacks)
+    navbar = NavBar.NavBar(window, navbar_callbacks)
     navbar.pack(fill=tkinter.X, expand=True)
 
     window.pack(fill=tkinter.BOTH, expand=True)
