c5be6b4a4fd53846b65c85048f92958df4b83100
Nick Vazquez <nmv@iastate.edu>
2022-12-04 16:11:24 -0600

    Remove unused control buttons and add quit logic

--- 288GUI/App.py
+++ 288GUI/App.py
@@ -1,4 +1,5 @@
 import logging
+import signal
 import tkinter as tk
 import tkinter.ttk as ttk
 import Components.NavBar as NavBar
@@ -16,61 +17,70 @@ app_screen_height_pct = 75
 logger = logging.getLogger()
 
 
-def setup_callbacks():
-    nav_callbacks = NavSectionCallbacks(lambda x=0: print('Home'),
-                                        lambda x=0: print('Console'),
-                                        lambda x=0: print('About'))
-    control_callbacks = ControlSectionCallbacks(lambda x=0: print('Start'),
-                                                lambda x=0: print('Stop'),
-                                                lambda x=0: print('Pause'),
-                                                lambda x=0: print('Reset'),
-                                                lambda x=0: print('EStop'))
-    movement_callbacks = MovementCallbacks(lambda x=0: print("Forward"),
-                                           lambda x=0: print("Reverse"),
-                                           lambda x=0: print("Left"),
-                                           lambda x=0: print("Right"))
+class App:
+    def __init__(self, root: tk.Tk):
+        # Setup window and nav bar
+        self.root = root
+        style = ttk.Style(self.root)
+        style.theme_use('clam')
 
-    navbar_callbacks = NavBarCallbacks(nav_callbacks, control_callbacks)
+        width = self.root.winfo_screenwidth() * (app_screen_width_pct / 100)
+        height = self.root.winfo_screenheight() * (app_screen_height_pct / 100)
+        self.root.geometry(f"{int(width)}x{int(height)}")
+        self.root.title("Parking Buddy")
 
-    return navbar_callbacks, movement_callbacks
+        window = tk.Frame(self.root)
+
+        navbar_callbacks, movement_callbacks = self.setup_callbacks()
+        navbar = NavBar.NavBar(window, navbar_callbacks)
+        navbar.pack(fill=tk.X, expand=True)
+        button = Buttons.MovementButtons(window, movement_callbacks)
+        button.pack()
+
+        console = ConsoleUi(window)
+        console.pack()
+        logger.addHandler(console.queue_handler)
+
+        scan_result = Results.ScanResult()
+        scan_result.result = [i / 2 for i in range(90)]
+        plotter = Plotter.PlotterView(window, scan_result)
+        plotter.pack()
+
+        occupancy_widget = BayOccupancyWidget(window)
+        occupancy_widget.occupancies = [3, 4, 5, 6]
+        occupancy_widget.pack()
+
+        window.pack(fill=tk.BOTH, expand=True)
+
+        self.root.protocol('WM_DELETE_WINDOW', self.quit)
+        self.root.bind('<Control-q>', self.quit)
+        signal.signal(signal.SIGINT, self.quit)
+
+    @staticmethod
+    def setup_callbacks():
+        nav_callbacks = NavSectionCallbacks(lambda x=0: print('Home'),
+                                            lambda x=0: print('Console'),
+                                            lambda x=0: print('About'))
+        control_callbacks = ControlSectionCallbacks(lambda x=0: print('Start'),
+                                                    lambda x=0: print('Stop'))
+        movement_callbacks = MovementCallbacks(lambda x=0: print("Forward"),
+                                               lambda x=0: print("Reverse"),
+                                               lambda x=0: print("Left"),
+                                               lambda x=0: print("Right"))
+
+        navbar_callbacks = NavBarCallbacks(nav_callbacks, control_callbacks)
+
+        return navbar_callbacks, movement_callbacks
+
+    def quit(self, *args):
+        self.root.destroy()
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
-
-    # Setup window and nav bar
     root = tk.Tk()
-    style = ttk.Style(root)
-    style.theme_use('clam')
-
-    width = root.winfo_screenwidth() * (app_screen_width_pct / 100)
-    height = root.winfo_screenheight() * (app_screen_height_pct / 100)
-    root.geometry(f"{int(width)}x{int(height)}")
-    root.title("Parking Buddy")
-
-    window = tk.Frame(root)
-
-    navbar_callbacks, movement_callbacks = setup_callbacks()
-    navbar = NavBar.NavBar(window, navbar_callbacks)
-    navbar.pack(fill=tk.X, expand=True)
-    button = Buttons.MovementButtons(window, movement_callbacks)
-    button.pack()
-
-    console = ConsoleUi(window)
-    console.pack()
-    logger.addHandler(console.queue_handler)
-
-    scan_result = Results.ScanResult()
-    scan_result.result = [i / 2 for i in range(90)]
-    plotter = Plotter.PlotterView(window, scan_result)
-    plotter.pack()
-
-    occupancy_widget = BayOccupancyWidget(window)
-    occupancy_widget.occupancies = [3, 4, 5, 6]
-    occupancy_widget.pack()
-
-    window.pack(fill=tk.BOTH, expand=True)
-    root.mainloop()
+    app = App(root)
+    app.root.mainloop()
 
 
 if __name__ == '__main__':
